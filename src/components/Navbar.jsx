0b342cb723c43e7ad9a63196fe9b8ae4604e9959
Lawcito <121973056+Lawcito@users.noreply.github.com>
2024-08-29 16:49:08 -0300

    skills

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,24 @@ const routes = [
 
 function Navbar() {
   return (
-    <nav className="lg:block hidden h-14 bg-[#ECA72C]">
-      <ul className="flex justify-around items-center h-full text-[#221E22] font-serif">
-        <li>
-          <Link to="/">
-            <img src={logo} alt="logo" className="w-8 h-8" />
-          </Link>
-        </li>
-        {routes.map(({ path, label }, index) => {
-          return (
-            <li key={index} className="hover:text-[#44355B] hover:underline">
-              <Link to={path}>{label}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <>
+      <nav className="md:block hidden h-14 bg-[#ECA72C]">
+        <ul className="flex justify-around items-center h-full text-[#221E22] font-serif">
+          <li>
+            <Link to="/">
+              <img src={logo} alt="logo" className="w-8 h-8" />
+            </Link>
+          </li>
+          {routes.map(({ path, label }, index) => {
+            return (
+              <li key={index} className="hover:text-[#44355B] hover:underline">
+                <Link to={path}>{label}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
   );
 }
 
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,45 +1,133 @@
+import javascriptLogo from "../assets/javascriptLogo.png";
+import reactLogo from "../assets/reactLogo.png";
+import tailwindcssLogo from "../assets/tailwindcssLogo.png";
+import daisyuiLogo from "../assets/daisyuiLogo.png";
+import CustomBtn from "../components/CustomBtn";
+
 function Skills() {
   return (
-    <div className="h-full w-full bg-[]">
+    <div className="h-full w-full flex flex-col justify-center">
       <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
+        <div
+          id="jsLogo"
+          className="carousel-item w-full flex items-center justify-center flex-col gap-2"
+        >
           <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
+            src={javascriptLogo}
+            className="h-32 w-32 border-4 border-[#ED7F27] rounded-full"
           />
+          {/*Texto informativo*/}
+          <div>
+            <h3 className="text-[#ECA72C] font-serif underline text-center">
+              Javascript
+            </h3>
+            <div className="w-80 h-32 overflow-auto overflow-auto-y scrollbar-custom bg-[#ECA72C] border rounded border-[#ECA72C] p-2">
+              <p className="text-center text-[#221E22] font-serif">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Suscipit dolores tempora omnis? Eum exercitationem nihil
+                officiis, deserunt repudiandae, quae similique voluptatibus ex
+                earum unde, quibusdam consequuntur dolores? Dolorum, eligendi
+                aliquid.
+              </p>
+            </div>
+          </div>
         </div>
-        <div id="item2" className="carousel-item w-full">
+
+        <div
+          id="reactLogo"
+          className="carousel-item w-full flex items-center justify-center flex-col gap-2"
+        >
           <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full"
+            src={reactLogo}
+            className="h-32 w-32 border-4 border-[#ED7F27] rounded-full"
           />
+          {/*Texto informativo*/}
+          <div>
+            <h3 className="text-[#ECA72C] font-serif underline text-center">
+              React
+            </h3>
+            <div className="w-80 h-32 overflow-auto overflow-auto-y scrollbar-custom bg-[#ECA72C] border rounded border-[#ECA72C] p-2">
+              <p className="text-center text-[#221E22] font-serif">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Suscipit dolores tempora omnis? Eum exercitationem nihil
+                officiis, deserunt repudiandae, quae similique voluptatibus ex
+                earum unde, quibusdam consequuntur dolores? Dolorum, eligendi
+                aliquid.
+              </p>
+            </div>
+          </div>
         </div>
-        <div id="item3" className="carousel-item w-full">
+        <div
+          id="tailwindcssLogo"
+          className="carousel-item w-full flex items-center justify-center flex-col gap-2"
+        >
           <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full"
+            src={tailwindcssLogo}
+            className="h-32 w-32 border-4 border-[#ED7F27] rounded-full"
           />
+          {/*Texto informativo*/}
+          <div className="items">
+            <h3 className="text-[#ECA72C] font-serif underline text-center">
+              Tailwind Css
+            </h3>
+            <div className="w-80 h-32 overflow-auto overflow-auto-y scrollbar-custom bg-[#ECA72C] border rounded border-[#ECA72C] p-2">
+              <p className="text-center text-[#221E22] font-serif">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Su
+              </p>
+            </div>
+          </div>
         </div>
-        <div id="item4" className="carousel-item w-full">
+        <div
+          id="daisyuiLogo"
+          className="carousel-item w-full flex items-center justify-center flex-col gap-2"
+        >
           <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full"
+            src={daisyuiLogo}
+            className="h-32 w-32 border-4 border-[#ED7F27] rounded-full"
           />
+          {/*Texto informativo*/}
+          <div>
+            <h3 className="text-[#ECA72C] font-serif underline text-center">
+              DaisyUi
+            </h3>
+            <div className="w-80 h-32 overflow-auto overflow-auto-y scrollbar-custom bg-[#ECA72C] border rounded border-[#ECA72C] p-2">
+              <p className="text-center text-[#221E22] font-serif">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Suscipit dolores tempora omnis? Eum exercitationem nihil
+                officiis, deserunt repudiandae, quae similique voluptatibus ex
+                earum unde, quibusdam consequuntur dolores? Dolorum, eligendi
+                aliquid.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-        <a href="#item4" className="btn btn-xs">
-          4
-        </a>
+        <CustomBtn
+          type="submit"
+          text="1"
+          carousel
+          onClick={() => (window.location.href = "#jsLogo")}
+        />
+        <CustomBtn
+          type="submit"
+          text="2"
+          carousel
+          onClick={() => (window.location.href = "#reactLogo")}
+        />
+        <CustomBtn
+          type="submit"
+          text="3"
+          carousel
+          onClick={() => (window.location.href = "#tailwindcssLogo")}
+        />
+        <CustomBtn
+          type="submit"
+          text="4"
+          carousel
+          onClick={() => (window.location.href = "#daisyuiLogo")}
+        />
       </div>
     </div>
   );
